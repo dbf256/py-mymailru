eade5493bf4196641c51624c6a085b7e09a3a9ef
dbf256@gmail.com <dbf256@gmail.com@4cc57575-4a55-4652-e775-a68c32a5433a>
2011-08-28 16:59:25 +0000

    Example was improved

--- example.py
+++ example.py
@@ -4,8 +4,8 @@ from pymymailru.pymymailru import PyMyMailRu, ApiError
 # инициализация - передаем id приложения, секретный ключ и задаем формат выдачи
 py_my_mail_ru = PyMyMailRu(123456, 'sf2u8jedfgdfglrjlht58', 'xml')
 try:
-    # получаем информацию о пользователе 1234567 от лица пользователя 7654321
-    result = py_my_mail_ru.users_get_info(1234567, 7654321)
+    # получаем информацию о пользователях 1234567, 1234568 от лица пользователя 7654321
+    result = py_my_mail_ru.users_get_info('1234567,1234568', 7654321)
     print result
 # обработка ошибок
 except  ApiError, e:

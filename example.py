1271c125db60548128323e9bec8af97d07c9fe5e
dbf256@gmail.com <dbf256@gmail.com@4cc57575-4a55-4652-e775-a68c32a5433a>
2011-08-28 16:57:35 +0000

    Comments to example script were added

--- example.py
+++ example.py
@@ -1,10 +1,13 @@
-# -*- coding: utf-8 -*-
+# импорт
 from pymymailru.pymymailru import PyMyMailRu, ApiError
 
-py_my_mail_ru = PyMyMailRu(123456, 'rewrewsdf345345546546', 'xml')
+# инициализация - передаем id приложения, секретный ключ и задаем формат выдачи
+py_my_mail_ru = PyMyMailRu(123456, 'sf2u8jedfgdfglrjlht58', 'xml')
 try:
+    # получаем информацию о пользователе 1234567 от лица пользователя 7654321
     result = py_my_mail_ru.users_get_info(1234567, 7654321)
     print result
+# обработка ошибок
 except  ApiError, e:
     print e.code
     print e.message
